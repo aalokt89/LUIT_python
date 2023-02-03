6a522d5f3decd325526c7e046b19604656c7aeee
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-03 20:59:09 +0000

    clean up and separate functions

--- WK_12/luit_wk12_randomNames.py
+++ WK_12/luit_wk12_randomNames.py
@@ -2,11 +2,26 @@ import random
 import string
 
 nameList = []
-
 deptOptions = ["Marketing", "Accounting", "FinOps"]
 
 
-def randomName(deptName, nameCount):
+def initAsk():
+    deptName = input("Enter department name (Marketing, Accounting, or FinOps): ")
+    
+    #validate dept name
+    if deptName.lower() in str(deptOptions).lower():
+        nameCount = int(input("Enter number of names to generate: "))
+        createName(deptName, nameCount)
+        
+        print(nameList)
+        return 0
+        
+    else:
+        print(f"'{deptName}' is not a valid department. You must choose between 'Marketing, Accounting, or FinOps.'")
+        return 1
+    
+#functiion to create random names___________________________________________
+def createName(deptName, nameCount):
     
     #iterate over nameCount
     for i in range(nameCount):
@@ -18,27 +33,11 @@ def randomName(deptName, nameCount):
         #newName = ((deptName, suffix))
         
         #join deptName and random suffix into one string
-        finalName = str(f"{deptName}_{suffix}") 
+        finalName = str(f"{deptName.lower()}_{suffix}") 
         
-        #add finalNamee to nameList
+        #add finalName to nameList
         nameList.append(finalName)
     
     return nameList
     
-#check valid dept option and execute randomize if true
-def deptChecker():
-    deptName = input("Enter department name: ").lower()
-    nameCount = int(input("Enter number of names to generate: "))
-    
-    if deptName in str(deptOptions).lower():
-        randomName(deptName, nameCount)
-        return True
-    else:
-        print(f"'{deptName}' is not a valid option.")
-        return False
-        
-
-
-
-deptChecker()    
-print(nameList)
+initAsk()
