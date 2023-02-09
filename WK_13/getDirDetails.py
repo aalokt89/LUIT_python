819f06b91c0ae431c654ad1af8c4cfbb5f0f7dcc
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-08 22:36:44 -0500

    update function

--- WK_13/getDirDetails.py
+++ WK_13/getDirDetails.py
@@ -6,17 +6,25 @@ def getDirDetails(path=os.getcwd()):
     myFiles = os.listdir(path)
     fileList = []
 
-    for file in myFiles:
-        filePath = os.path.realpath(file)
-        fileSize = os.path.getsize(file)
-
-        fileInfo = {'path': filePath, 'size:': fileSize}
-
-        fileList.append(fileInfo)
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            fileDict = {'path': os.path.join(
+                root, file), 'size': os.path.getsize(os.path.join(root, file))}
+            fileList.append(fileDict)
 
     return fileList
 
+    # for file in myFiles:
+    #     filePath = os.path.realpath(file)
+    #     fileSize = os.path.getsize(file)
 
-pathFiles = getDirDetails()
+    #     fileInfo = {'path': filePath, 'size:': fileSize}
+
+    #     fileList.append(fileInfo)
+
+    # return fileList
+
+
+pathFiles = getDirDetails("/Users/aaloktrivedi/LUIT/Python")
 
 print(pathFiles)
