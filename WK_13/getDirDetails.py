06e1bdd3ac2cca33f59833c97a98d9e3ab1c1b23
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-09 22:48:49 -0500

    added json output and more error handling

--- WK_13/getDirDetails.py
+++ WK_13/getDirDetails.py
@@ -1,29 +1,60 @@
 #!/usr/bin/env python3
 import os
+import datetime
+import json
+
+# convert time to human readable format
+
+
+def convertDate(timestamp):
+    d = datetime.datetime.utcfromtimestamp(timestamp)
+    formatedDate = d.strftime('%b %d, %Y')
+
+    return formatedDate
 
 
 def getDirDetails(path=os.getcwd()):
     path = input("Enter path: ")
     fileList = []
-    pathExists = os.path.exists(path)
 
-    if pathExists:
+    pathExists = os.path.exists(path)
+    isFile = os.path.isfile(path)
+    isDir = os.path.isdir(path)
+
+    if pathExists and isDir:
         for root, dirs, files in os.walk(path):
             for file in files:
+                # get file path
                 filePath = os.path.join(root, file)
-                fileSize = os.path.getsize(os.path.join(root, file))
+                # get file size
+                fileSize = round(os.path.getsize(filePath) / 1024, 1)
+                # get and convert creation date
+                fileCreationDate = convertDate(os.path.getctime(filePath))
 
-                fileDict = {'path': filePath, 'size': fileSize}
+                # add file details into a dict
+                fileDict = {
+                    'File Name': file,
+                    'Path': filePath,
+                    'Size(kb)': fileSize,
+                    "Date Created": fileCreationDate
+                }
+
+                # add file dicts into the list
                 fileList.append(fileDict)
 
-        return fileList
+                # json of file
+                pathFilesJSON = json.dumps(fileList, indent=4)
 
-    else:
+        return pathFilesJSON
+
+    elif isFile:
+        print(f"Error: The path '{path}' must be a directory.")
+
+    elif pathExists == False:
         print(f"Error: The path '{path}' does not exist.")
-        getDirDetails()
 
 
+# list of files/info in root path
 pathFiles = getDirDetails()
 
-for i in pathFiles:
-    print(i)
+print(pathFiles)
