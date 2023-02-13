9f8fbff7760900fdabd388ad215f3bbeebfde737
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-13 06:11:04 +0000

    upload file, delete file, delete bucket

--- AWS/create_s3.py
+++ AWS/create_s3.py
@@ -1,13 +1,30 @@
 import boto3
 
+#connect to s3
 s3_client = boto3.client("s3")
+s3 = boto3.resource('s3')
 
+
+#create bucket
+bucketName = "atrivedi89-bucket-1"
 # s3_client.create_bucket(
-#     Bucket = "atrivedi89-bucket-1"
+#     Bucket = bucketName
     
 #     )
 
+#list buckets
 response = s3_client.list_buckets()
 
 for bucket in response['Buckets']:
     print(bucket)
+
+#upload file
+s3.Bucket(bucketName).upload_file('/tmp/hello.txt', 'hello.txt')
+
+#delete object
+s3.Object(bucketName, 'hello.text').delete()
+
+#delete bucket
+respsonse = s3_client.deleteBucket(
+    Bucket = bucketName
+    )
