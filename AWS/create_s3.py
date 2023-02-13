a0388b04f75b3c266bfef428f080850004875855
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-13 19:22:30 +0000

    edit bucket listing for resource

--- AWS/create_s3.py
+++ AWS/create_s3.py
@@ -6,25 +6,29 @@ s3 = boto3.resource('s3')
 
 
 #create bucket
-bucketName = "atrivedi89-bucket-1"
+# bucketName = "atrivedi89-bucket-1"
 # s3_client.create_bucket(
 #     Bucket = bucketName
     
 #     )
 
-#list buckets
+#list buckets-client
 response = s3_client.list_buckets()
 
 for bucket in response['Buckets']:
-    print(bucket)
+    print(bucket["Name"])
+
+#list buckets-resource
+for bucket in s3.buckets.all():
+    print(bucket.name)
 
 #upload file
-s3.Bucket(bucketName).upload_file('/tmp/hello.txt', 'hello.txt')
+#s3.Bucket(bucketName).upload_file('/tmp/hello.txt', 'hello.txt')
 
 #delete object
-s3.Object(bucketName, 'hello.text').delete()
+#s3.Object(bucketName, 'hello.text').delete()
 
 #delete bucket
-respsonse = s3_client.deleteBucket(
-    Bucket = bucketName
-    )
+# respsonse = s3_client.deleteBucket(
+#     Bucket = bucketName
+#     )
