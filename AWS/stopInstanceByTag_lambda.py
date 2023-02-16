29f5ff7b68e95de86f1441aafce3d009df9cc52c
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-16 15:26:38 -0500

    updated efficiency of lambda function

---
+++ AWS/stopInstanceByTag_lambda.py
@@ -0,0 +1,36 @@
+import boto3
+from pprint import pprint
+import time
+
+ec2 = boto3.client('ec2', region_name='us-east-1')
+
+
+def lambda_handler():
+    filteredInstances = []
+
+    ec2Filters = [
+        {
+            'Name': 'tag:Environment',
+            'Values': ['Dev']
+        },
+        {
+            'Name': 'instance-state-name',
+            'Values': ['running']
+        }]
+
+    # get instances by tag and 'running' state
+    instances = ec2.describe_instances(Filters=ec2Filters)
+
+    # get filtered instance ids and add them to runningInstances
+    for each in instances['Reservations']:
+        for instance in each['Instances']:
+            filteredInstances.append(instance['InstanceId'])
+
+    if len(filteredInstances) > 0:
+        ec2.stop_instances(InstanceIds=filteredInstances)
+        print(f"Successfully stopped {len(filteredInstances)} instances.")
+    else:
+        print("There are no running instances with the selected filters to stop.")
+
+
+lambda_handler()
