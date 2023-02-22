8d4ae2ee347d618e96ca1a58066de8c32f3a65fa
Aalok Trivedi <aaloktrivedi89@gmail.com>
2023-02-21 19:05:06 -0500

    add env variables

--- WK_15/getDate_uuid_lambda.py
+++ WK_15/getDate_uuid_lambda.py
@@ -1,33 +1,32 @@
 import json
 from datetime import datetime
 import uuid
+import os
 import boto3
 
-# store api routes
-GET_DATETIME = "/datetime"
-GET_UUID = "/random-uuid"
+# set env variables
+DATETIME = os.environ['GET_DATETIME']
+UUID = os.environ['GET_UUID']
+queue = os.environ['QUEUE']
 
 
 def lambda_handler(event, context):
     apiEvent = event['rawPath']
     sqs = boto3.resource('sqs')
 
-    myQueue = sqs.Queue(
-        'https://sqs.us-east-1.amazonaws.com/054301730155/DateTime_UUID_sqs')
-
     try:
-        if apiEvent == GET_DATETIME:
+        if apiEvent == DATETIME:
             currentDateTime = datetime.strftime(
                 datetime.now(), "%d/%m/%Y, %H:%M:%S")
             message = f"Current time: {currentDateTime}."
             print(message)
 
-        elif apiEvent == GET_UUID:
+        elif apiEvent == UUID:
             randUUID = str(uuid.uuid4())
             message = f"Random uuid: {randUUID}."
             print(message)
 
-        response = myQueue.send_message(
+        response = queue.send_message(
             MessageBody=message
         )
         return response
